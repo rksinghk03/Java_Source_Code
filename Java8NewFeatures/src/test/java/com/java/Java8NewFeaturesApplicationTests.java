4e5a9d106a8afd475da7439e5fe4088ba5a6da5a
RajSingh <RajSingh@9.102.53.78>
2020-03-08 13:31:05 +0530

    Java 8

---
+++ Java8NewFeatures/src/test/java/com/java/Java8NewFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8NewFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
