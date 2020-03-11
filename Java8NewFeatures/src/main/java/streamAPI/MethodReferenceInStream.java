a5ed82968273ef99d9686a34c8eed047eeb55453
RajSingh <RajSingh@9.102.52.34>
2020-03-11 22:51:28 +0530

    Java 8 New Features

---
+++ Java8NewFeatures/src/main/java/streamAPI/MethodReferenceInStream.java
@@ -0,0 +1,50 @@
+package streamAPI;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Product1{  
+    int id;  
+    String name;  
+    float price;  
+      
+    public Product1(int id, String name, float price) {  
+        this.id = id;  
+        this.name = name;  
+        this.price = price;  
+    }  
+          
+    public int getId() {  
+        return id;  
+    }  
+    public String getName() {  
+        return name;  
+    }  
+    public float getPrice() {  
+        return price;  
+    }  
+}  
+  
+public class MethodReferenceInStream {  
+  
+    public static void main(String[] args) {  
+          
+        List<Product1> productsList = new ArrayList<Product1>();  
+          
+        //Adding Products  
+        productsList.add(new Product1(1,"HP Laptop",25000f));  
+        productsList.add(new Product1(2,"Dell Laptop",30000f));  
+        productsList.add(new Product1(3,"Lenevo Laptop",28000f));  
+        productsList.add(new Product1(4,"Sony Laptop",28000f));  
+        productsList.add(new Product1(5,"Apple Laptop",90000f));  
+          
+        List<Float> productPriceList =   
+                productsList.stream()  
+                            .filter(p -> p.price > 30000) // filtering data  
+                            .map(Product1::getPrice)         // fetching price by referring getPrice method  
+                            .collect(Collectors.toList());  // collecting as list  
+        System.out.println(productPriceList);  
+    }  
+}
+
